923c3225ff907d5d6fcabd92c2134ba13db0cfe5
shinseitaro <shinsei.all@gmail.com>
2020-10-31 08:00:44 +0900

    add shinseitaro dir

--- cookbook/submit/shinseitaro/elementary_01.py
+++ cookbook/submit/shinseitaro/elementary_01.py
@@ -1,5 +1,10 @@
+# 1. 1から10の整数をリストを`変数x`に格納して printしてください。ただし range 関数を使うこと。さらに全要素を2乗したリストを`変数y`に格納して printしてください。
+
 def test():
-    pass 
+    x = list(range(1,11))
+    y = [i**2 for i in x]
+    print(x)
+    print(y)
 
 if __name__ == "__main__":
     test()
